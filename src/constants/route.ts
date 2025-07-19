ecf9da714ada892c80a0314e72d2f5f05cc70837
Jihoon Lee <anteater1056@gmail.com>
2025-07-19 16:03:40 +0900

    init: 프론트 라우팅 경로 정의

---
+++ src/constants/route.ts
@@ -0,0 +1,26 @@
+const SERVICE_ROUTE = {
+  INDEX: '/',
+  LOGIN: {
+    INDEX: '/login',
+    PASSWORD: '/login/password',
+  },
+  SIGNUP: {
+    INDEX: '/signup',
+    DONE: '/signup/done',
+  },
+  TIMELINE: {
+    INDEX: '/timeline',
+    CREATE: '/timeline/create',
+    DONE: '/timeline/create/done',
+    HOME: '/timeline/$timelineId',
+    SESSION: {
+      CREATE: '/session/create',
+      HOME: '/session/$sessionId',
+      EDIT: '/session/$sessionId/edit',
+    },
+    SETTING: {
+      INDEX: '/timeline/$timelineId/setting',
+      MEMBER: '/timeline/$timelineId/setting/member',
+    },
+  },
+}
