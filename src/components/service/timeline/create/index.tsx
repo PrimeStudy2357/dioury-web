31c9531d2b67b5ec9c78664c8611dfd77faab16b
Jihoon Lee <anteater1056@gmail.com>
2026-05-17 21:04:18 +0900

    feat: 타임라인 생성 form 데이터 연결

--- src/components/service/timeline/create/index.tsx
+++ src/components/service/timeline/create/index.tsx
@@ -1,9 +1,25 @@
+import { useState } from 'react';
+
 export const TimelineCreate = () => {
+  const [isUnique, setIsUnique] = useState(false);
+
+  const handleCheckName = (formData: FormData) => {
+    const name = formData.get('name') as string;
+    alert(name);
+  };
+
+  const handleCreateAction = (formData: FormData) => {
+    formData.forEach((value, key) => {
+      console.log(`${key}: ${value}`);
+    });
+  };
+
   return (
-    <div className="pt-8 px-6 flex flex-col gap-6">
-      <h1 className="text-4xl font-bold">타임라인 만들기</h1>
-      <table
-        className="table-fixed border-t-2 border-b-2
+    <form action={handleCreateAction}>
+      <div className="pt-8 px-6 flex flex-col gap-6">
+        <h1 className="text-4xl font-bold">타임라인 만들기</h1>
+        <table
+          className="table-fixed border-t-2 border-b-2
         [&_tr]:text-2xl
         [&_tr]:h-18
         [&_tr]:border-b
@@ -11,102 +27,121 @@ export const TimelineCreate = () => {
         [&_td_div]:flex
         [&_input]:border-2
       "
-      >
-        <tbody>
-          <tr>
-            <td>타임라인 이름</td>
-            <td>
-              <input className="w-full" />
-            </td>
-            <td className="text-right">
-              <button className="bg-black text-white px-10 py-1 text-2xl font-bold">
-                중복 확인
-              </button>
-            </td>
-          </tr>
-          <tr>
-            <td>공개 여부</td>
-            <td>
-              <div>
-                <label>
-                  <input type="radio" name="public" />
-                  <span>공개</span>
-                </label>
-                <label>
-                  <input type="radio" name="public" />
-                  <span>비공개</span>
-                </label>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>프로필 사진</td>
-            <td>
-              <div>
-                <div>Placeholder</div>
+        >
+          <tbody>
+            <tr>
+              <td>타임라인 이름</td>
+              <td>
+                <input name="name" className="w-full" />
+              </td>
+              <td className="text-right">
+                <button
+                  type="submit"
+                  formAction={handleCheckName}
+                  className="bg-black text-white px-10 py-1 text-2xl font-bold"
+                >
+                  중복 확인
+                </button>
+              </td>
+            </tr>
+            <tr>
+              <td>공개 여부</td>
+              <td>
                 <div>
-                  ㅁ너ㅏㅣㄹㅁㄴ아ㅓㅣ럼ㄴ아ㅣㄹㄴㅁㅇㄹ (프로필 사진 안내사항)
+                  <label>
+                    <input
+                      type="radio"
+                      name="isPublic"
+                      value="public"
+                      defaultChecked
+                    />
+                    <span>공개</span>
+                  </label>
+                  <label>
+                    <input type="radio" name="isPublic" value="private" />
+                    <span>비공개</span>
+                  </label>
                 </div>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>카테고리</td>
-            <td>
-              <input />
-            </td>
-          </tr>
-          <tr>
-            <td>키워드</td>
-            <td>
-              <input />
-            </td>
-          </tr>
-          <tr>
-            <td>타임라인 설명</td>
-            <td>
-              <textarea />
-            </td>
-          </tr>
-          <tr>
-            <td>주기</td>
-            <td>
-              <div>
-                <label>
-                  <input type="radio" name="period" />
-                  <span>비정기</span>
-                </label>
-                <label>
-                  <input type="radio" name="period" />
-                  <span>정기</span>
-                </label>
+              </td>
+            </tr>
+            <tr>
+              <td>프로필 사진</td>
+              <td>
                 <div>
-                  <input />
-                  <span>ex. 주 1회</span>
+                  <div>Placeholder</div>
+                  <div>
+                    ㅁ너ㅏㅣㄹㅁㄴ아ㅓㅣ럼ㄴ아ㅣㄹㄴㅁㅇㄹ (프로필 사진
+                    안내사항)
+                  </div>
                 </div>
-              </div>
-            </td>
-          </tr>
-          <tr>
-            <td>온/오프라인</td>
-            <td>
-              <label>
-                <input type="radio" name="on" />
-                <span>온라인</span>
-              </label>
-              <label>
-                <input type="radio" name="on" />
-                <span>오프라인</span>
-              </label>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-      <div className="flex justify-end">
-        <button className="bg-black text-white px-10 py-1 text-3xl font-bold">
-          다 만듦
-        </button>
+              </td>
+            </tr>
+            <tr>
+              <td>카테고리</td>
+              <td>
+                <input name="category" />
+              </td>
+            </tr>
+            <tr>
+              <td>키워드</td>
+              <td>
+                <input name="keyword" />
+              </td>
+            </tr>
+            <tr>
+              <td>타임라인 설명</td>
+              <td>
+                <textarea name="description" />
+              </td>
+            </tr>
+            <tr>
+              <td>주기</td>
+              <td>
+                <div>
+                  <label>
+                    <input
+                      type="radio"
+                      name="isRegular"
+                      value="irregular"
+                      defaultChecked
+                    />
+                    <span>비정기</span>
+                  </label>
+                  <label>
+                    <input type="radio" value="regular" name="isRegular" />
+                    <span>정기</span>
+                  </label>
+                  <div>
+                    <input name="period" />
+                    <span>ex. 주 1회</span>
+                  </div>
+                </div>
+              </td>
+            </tr>
+            <tr>
+              <td>온/오프라인</td>
+              <td>
+                <label>
+                  <input type="radio" name="isOn" value="on" defaultChecked />
+                  <span>온라인</span>
+                </label>
+                <label>
+                  <input type="radio" name="isOn" value="off" />
+                  <span>오프라인</span>
+                </label>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="flex justify-end">
+          <button
+            type="submit"
+            className="cursor-pointer bg-black text-white px-10 py-1 text-3xl font-bold"
+          >
+            다 만듦
+          </button>
+        </div>
       </div>
-    </div>
+    </form>
   );
 };
